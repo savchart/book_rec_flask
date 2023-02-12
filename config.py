d3c7a33b1b5885c1a793080e9ec291993cbb032b
savchart <savchart@fit.cvut.cz>
2023-02-12 17:15:17 +0100

    App testing

--- config.py
+++ config.py
@@ -2,8 +2,10 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+
 class Config:
+    DF_PATH = os.path.join(basedir, 'data/df.csv')
     DEBUG = True
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')\
-        or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
